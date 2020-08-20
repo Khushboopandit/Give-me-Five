de50b38516c3fe3593f63af0967819b2fb93dde1
Khushboo pandit <khushboo17@navgurukul.org>
2020-08-20 17:53:47 +0530

    Updates

---
+++ precache-manifest.c8aec0a27a9f862b9677213df06ca938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e78c1c964602d5facc8784ade0590e8",
+    "url": "/Give-me-Five/index.html"
+  },
+  {
+    "revision": "66b40d8d3ec95d070284",
+    "url": "/Give-me-Five/static/css/main.93afa5bd.chunk.css"
+  },
+  {
+    "revision": "0f1aca7e69732624321b",
+    "url": "/Give-me-Five/static/js/2.9c50009e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Give-me-Five/static/js/2.9c50009e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b40d8d3ec95d070284",
+    "url": "/Give-me-Five/static/js/main.f1060964.chunk.js"
+  },
+  {
+    "revision": "baa60a578f909d208936",
+    "url": "/Give-me-Five/static/js/runtime-main.907a2e04.js"
+  }
+]);
